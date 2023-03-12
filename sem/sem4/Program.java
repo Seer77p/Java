0446a3112097f2897807c6b7a9109adc21225494
Seer77p <seer77p@mail.ru>
2023-03-12 12:35:08 +0300

    seminar4

---
+++ sem/sem4/Program.java
@@ -0,0 +1,5 @@
+package Java.sem.sem4;
+
+public class Program {
+
+}
---
+++ sem/sem4/sem4.java
@@ -0,0 +1,97 @@
+package Java.sem.sem4;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+// import java.util.logging.Logger;
+// import java.lang.System.Logger.Level;
+// import java.util.logging.FileHandler;
+
+public class sem4 {
+
+    public static void main(String[] args) {
+        //     Logger logger = Logger.getLogger(Program.class.getName());
+        // //            FileHandler fh = new FileHandler("file.txt");
+        //             logger.config("text.txt");
+        // //            logger.doLog(Level.INFO, "INFO");
+        // }
+        //измерение скорости
+        // ArrayList<Integer> arrayList = new ArrayList<>();
+        // long startTime = System.nanoTime();
+        // for(int i=0;i<1000000;i++)
+        //     arrayList.add(i);
+        // long endTime = System.nanoTime();
+        // System.out.printf("ArrayList spent time: %d\n", endTime - startTime);
+        // LinkedList<Integer> linkedList = new LinkedList<>();
+        // startTime = System.nanoTime();
+        // for(int i=0;i<1000000;i++)
+        //     linkedList.addFirst(i);
+        // endTime = System.nanoTime();
+        // System.out.printf("Linken spent    time: %d\n", endTime - startTime);
+
+        //Задачи
+        // Реализовать консольное приложение, которое:
+        // Принимает от пользователя строку вида 
+        // text~num
+        // Нужно рассплитить строку по ~, сохранить text 
+        //в связный список на позицию num.
+        // Если введено print~num, выводит строку из позиции 
+        //num в связном списке и удаляет её из списка.
+        // Scanner scanner = new Scanner(System.in);
+        // LinkedList<String> list = new LinkedList<>();
+        // list.add("hello");
+        // list.add("hi");
+        // list.add("text");
+        // list.add("text");
+        // while (true) {
+        //     System.out.print("enter string: ");
+        //     String text = scanner.nextLine(); // "print~0";
+        //     String[] array = text.split("~");
+        //     int i;
+        //     try {
+        //         i = Integer.parseInt(array[1]);
+        //     } catch (Exception e) {
+        //         System.out.println("error");
+        //         continue;
+        //     }
+        //     if ((array.length > 1) && (i > list.size() - 1))
+        //         System.out.println("error");
+        //     else if (array[0].equals("print"))
+        //         System.out.println(list.remove(i));
+        //     else
+        //         list.add(i, array[0]);
+        //     System.out.println("\n" + Arrays.asList(array) + text.toString() + list.toString());
+        //     scanner.close();
+
+
+        // LinkedList<String> linkedList = new LinkedList<>();
+        //     while (true) {
+        //         Scanner iscanner = new Scanner(System.in);
+        //     System.out.println("input your string: ");
+        //     String inputed1 = iscanner.nextLine();
+        //         // String inputed1="text~3";
+        //     // String inputed1="print~2";
+        //     String[] arr = inputed1.split("~", 0);
+        //     int pos = Integer.valueOf(arr[1])-1;
+        //     // System.out.println(Array.);
+        //     if (linkedList.isEmpty()){
+            
+        //     for (int index = 0; index < 12; index++) {
+        //         linkedList.add("-");
+        //     }}
+            
+        //     if (arr[0].equals("print")){
+        //         System.out.println(linkedList.get(pos-1));
+        //         linkedList.remove(pos-1);
+        //     } else {
+        //         linkedList.add(pos, arr[0]);}
+
+            
+        //     System.out.println(linkedList);
+        // iscanner.close();
+        // }
+    }
+}
